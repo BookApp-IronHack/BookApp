e134c2e669799db8814fa3296758719f207572a6
Antonioadv08 <antonioadv08@gmail.com>
2019-09-24 21:27:05 +0200

    j

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const User = require("../models/User");
+const Book = require("../models/Books");
+const upload = require("./../config/cloudinary.config");
+
 
 router.get('/profile', (req, res) => {
-const user=req.user
-    res.render('profile-detail',{user})
-  
+  const user = req.user
+  res.render('profile-detail', { user })
+
 })
 
-router.get('/reseña', (req, res) => {
-  const user=req.user
-      res.render('reseña',{user})
-    
-  })
+router.get('/resena', (req, res) => {
+  res.render('resena')
 
+})
+
+
+router.post("/books/:id", upload.single("userPhoto"), (req, res, next) => {
+  const { title, author, pages, publisher_date, categories, content } = req.body;
+  const { originalname, url } = req.file;
+
+  const newBook = new Book({
+    title,
+    author,
+    publisher_date,
+    categories,
+    content,
+    cover: {
+      url,
+      name: originalname
+    },
+    
+  });
+
+})
 
 
 module.exports = router;
