8fd906ac0ad2453b3faafd559a7a939e1cf644d3
Marta RoDi <rodriguezmdiaz@gmail.com>
2019-09-24 19:37:10 +0200

    all comments

--- models/Comments.js
+++ models/Comments.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  content: String
+  content: String,
+  creatorId: {type: Schema.Types.ObjectId, ref:'User'}
 },{
   timestamps: true,
 });
--- routes/books.js
+++ routes/books.js
@@ -1,46 +1,75 @@
 const express = require('express');
 const router = express.Router();
-const Books = require("../models/Books");
+const Books = require("../models/Book");
 const Comment = require("../models/Comments");
+const access = require("./../middlewares/access.mid");
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   Books.find()
-  .select({
-    cover: 1, title: 1, author: 1, categories: 1
-  })
-  .then(allBooks => {
-    console.log(allBooks)
-    res.render('index', {allBooks})
-  })
+    .select({
+      cover: 1,
+      title: 1,
+      author: 1,
+      categories: 1
+    })
+    .then(allBooks => {
+      res.render('index', {
+        allBooks
+      })
+    })
 })
 
 router.get('/books/:id/', (req, res) => {
+  const user = req.user
   Books.findById(req.params.id)
-  .populate('comments')
-  .then(oneBook => {
-    console.log(oneBook)
-    res.render('book-detail', {oneBook})
-  })
+    
+    .populate('comments')
+    .populate({ path: 'comments', populate: { path: 'creatorId' } })
+    .lean()
+    .then(oneBook => {
+      console.log(oneBook)
+     if(oneBook['comments']){
+      oneBook['comments'] = oneBook.comments.reverse();
+      res.render('book-detail', {
+        oneBook,
+        user
+      })
+     }else{
+      res.render('book-detail', {
+        oneBook,
+        user
+      })
+     }
+    })
 })
 
-router.post('/addComment/:bookId', (req, res) => {
-  const {content} = req.body;
-  const {bookId} = req.params;
-  const newComment = new Comment({
+router.post('/addComment/:bookId', access.checkLogin, (req, res) => {
+  const {
     content
+  } = req.body;
+  const {
+    bookId
+  } = req.params;
+  const newComment = new Comment({
+    content,
+    creatorId: req.user._id
   })
 
   newComment
-  .save()
-  .then((comment) => {
-    Books.findByIdAndUpdate(bookId, {$push: {comments: comment._id}})
-    .then(() => {
-      res.redirect('/');
+    .save()
+    .then((comment) => {
+      Books.findByIdAndUpdate(bookId, {
+          $push: {
+            comments: comment._id
+          }
+        })
+        .then(() => {
+          res.redirect('back');
+        })
+        .catch((error) => {
+          console.error(error);
+        })
     })
-    .catch((error) => {
-      next(error);
-    })
-  })
 })
 
 module.exports = router;
