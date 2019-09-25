08fc474fc08e5c6b718068cfff1b60b09f546672
Antonioadv08 <antonioadv08@gmail.com>
2019-09-25 16:27:42 +0200

    cambios profile

--- models/Books.js
+++ models/Books.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const bookSchema = new Schema({
+  creatorId: {type: Schema.Types.ObjectId, ref:'User'},
+
   cover: String,
   title: String,
   author: String,
